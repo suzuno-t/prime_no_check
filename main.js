2e60e09fdc2a076e99ffd439b70b229be2e83e25
suzuno-t <155156196+suzuno-t@users.noreply.github.com>
2024-04-07 09:42:33 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function generatePrimes() {
+  const maxValue = document.getElementById("max-value").value;
+  const primeList = document.getElementById("prime-list");
+  primeList.innerHTML = "";
+
+  for (let i = 2; i <= maxValue; i++) {
+    if (isPrime(i)) {
+      const li = document.createElement("li");
+      li.textContent = i;
+      primeList.appendChild(li);
+    }
+  }
+}
